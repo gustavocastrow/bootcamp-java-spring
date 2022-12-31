095e1ecf46c5f85f086ce2f8806ea5f3d729fd0f
Gustavo Castro <gustavocastrocs@gmail.com>
2022-12-31 12:12:45 -0300

    04.09 - Estruturas Repetitivas - DO WHILE

--- introducao/src/exemploFor/Main.java
+++ introducao/src/exemploFor/Main.java
@@ -10,10 +10,11 @@ public class Main {
 		Scanner sc = new Scanner(System.in);
 		
 		System.out.println("Quantidade de vezes do loop");
-		int N = sc.nextInt()	;
 		
+		int numero = sc.nextInt()	;
 		int soma = 0;
-		for (int i=0; i<N; i++) { //i=i+1
+		//inicio ; condição ; incremento
+		for (int i=0; i<numero; i++) { //i=i+1
 			int x = sc.nextInt();
 			soma += x;
 			
