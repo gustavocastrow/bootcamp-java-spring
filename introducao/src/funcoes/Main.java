40b5ad28ed5c745c321a877812782888f4ab2c69
Gustavo Castro <gustavocastrocs@gmail.com>
2022-12-31 12:37:26 -0300

    05.05 - Funções

---
+++ introducao/src/funcoes/Main.java
@@ -0,0 +1,43 @@
+package funcoes;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter three numbers: ");
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		
+		int higher = max(a, b, c);
+		showResult(higher);
+		
+	
+		sc.close();
+		
+	
+	}
+	
+	public static int max(int x, int y, int z) {
+		int aux;
+		if (x > y && x > z) {
+			aux = x;
+		} else if (y > z) {
+			aux = y;
+		} else {
+			aux = z;
+		}
+		
+		return aux;
+	}
+	
+	public static void showResult(int value) {
+		System.out.println("Higher = " + value);
+	}
+
+}
